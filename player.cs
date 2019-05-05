3c9b1d041f78ff50a2530855e929ff0dd3bd93d9
ShubhJaroria <shubh.jaroria@gmail.com>
2019-05-05 17:00:54 +0530

    Add files via upload

---
+++ player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("W", true);
+        }
+        else
+        {
+            anim.SetBool("W", false);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            anim.SetBool("S", true);
+        }
+        else
+        {
+            anim.SetBool("S", false);
+        }
+    }
+}
