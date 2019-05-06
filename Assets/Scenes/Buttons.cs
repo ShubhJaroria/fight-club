b05465a73518bdb878e138307cbd83a96bb6ffea
abhiaj <34074072+abhiaj@users.noreply.github.com>
2019-05-06 19:35:05 +0530

    Changes in buttons cs file
    Added documentation

--- Assets/Scenes/Buttons.cs
+++ Assets/Scenes/Buttons.cs
@@ -8,11 +8,13 @@ public class Buttons : MonoBehaviour
 
 //in the class:
 
+   // Reaction of the play button
 public void PlayGame()
 {
     SceneManager.LoadScene(1); //scene number to be updated
 }
 
+    //Reaction of the quit button
 public void QuitGame()
 {
     Application.Quit();
