0adcd8c391de81138e6f0ee4ec43b7f1636fe80b
abhiaj <34074072+abhiaj@users.noreply.github.com>
2019-05-07 04:25:33 +0530

    Documented Health and Health2
    Documented the codes that control the health bar of the two players.

--- health2.cs
+++ health2.cs
@@ -3,19 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/* This file controls the health of the first player.
+ * The update function sets the updated health each time the 
+ * first player is hit or its health decreases due to some other reason.*/
+
+//health class for the methods and global variables
 public class health2 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /*The public variable Slider takes in the element slider which is 
+   * used as a health bar. The variable myPlayer is the first player. 
+   * myHealth is the first player's health*/
     public Slider s;
     public player2 myPlayer;
     int myHealth;
-    // Update is called once per frame
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+        //nothing to be initialized in start 
     }
+
+    // Update is called once per frame
     void Update()
     {
+        /*updates the health every time the frame is loaded. the value of slider
+        * is updated accordingly and displayed on the screen.
+        * Note that the green area in slider represents the remaining health
+        * while the red region is the Damage taken.*/
         myHealth = myPlayer.health;
         s.value = myHealth;
 
