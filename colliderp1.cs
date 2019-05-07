74794b1e54a2cff95c0b0565f703a987ba7dd593
ShubhJaroria <shubh.jaroria@gmail.com>
2019-05-07 05:51:24 +0530

    Add files via upload

--- colliderp1.cs
+++ colliderp1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,45 +9,54 @@ public class colliderp1 : MonoBehaviour
     public int damageBody;
     public int damageLeg;
     public string name;
+    public int factor = 5;
     public string owner;
     public player2 p2;
     public player p1;
 
     // Update is called once per frame
+    
+
 
     void OnTriggerEnter(Collider other)
     {
-        colliderp1 c = other.gameObject.GetComponent<colliderp1>();
-        
-            if (c.owner == "p2" && p1.attack == true)
+        colliderp1 c2 = other.gameObject.GetComponent<colliderp1>();
+        colliderp1 c1 = this.gameObject.GetComponent<colliderp1>();
+       if (owner != c2.owner)
+        {
+           // Debug.Log(c1.owner + "'s " + c1.name + " hit " + c2.owner + "'s " + c2.name);
+        }
+            if (c2.gameObject.tag == "player2" && gameObject.tag == "player" && p1.attack == true)
           {
-            if (c.name == "head")
+            //Debug.Log(c.name);
+            if (c2.name == "head")
             {
-                p2.health -= damageHead;
+                p2.health -= damageHead/factor;
+                
             }
-               else if (c.name == "chest")
+               else if (c2.name == "chest")
             {
-                p2.health -= damageBody;
+                p2.health -= damageBody/factor;
             }
-               else if(c.name=="right leg" || c.name == "left leg")
+               else if(c2.name=="right leg" || c2.name == "left leg")
             {
-                p2.health -= damageLeg;
+                p2.health -= damageLeg/factor; 
             }
           }
-        if (c.owner == "p1" && p2.attack == true)
+        if (c2.owner == "p1" && p2.attack == true)
         {
             
             if (true)
             {
-                p1.health -= damageHead;
+                p1.health -= damageHead/factor;
             }
-            else if (c.name == "chest")
+            else if (c2.name == "chest")
             {
-                p1.health -= damageBody;
+                p1.health -= damageBody/factor;
             }
-            else if (c.name == "right leg" || c.name == "left leg")
+            else if (c2.name == "right leg" || c2.name == "left leg")
             {
-                p1.health -= damageLeg;
+                p1.health -= damageLeg/factor;
             }
         }
     }
